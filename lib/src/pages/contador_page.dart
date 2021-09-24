ce5a7f48c83bcc105c223c643177289b691b6dcf
51lv3r258 <jrp8900@gmail.com>
2021-09-24 00:07:13 -0500

    ✨ Add new buttons and logic for increase, decrease a reset the counter

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -15,32 +15,66 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('App Contador'),
-        centerTitle: true,
-        elevation: 10.0,
-        // foregroundColor: const Color.fromRGBO(3, 237, 249, 1),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Contador: Número de clicks',
-            style: _textStyle,
-          ),
-          Text(
-            '$_counter',
-            style: _textStyle,
-          )
-        ],
-      )),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
+        appBar: AppBar(
+          title: const Text('App Contador'),
+          centerTitle: true,
+          elevation: 10.0,
+          // foregroundColor: const Color.fromRGBO(3, 237, 249, 1),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Contador: Número de clicks',
+              style: _textStyle,
+            ),
+            Text(
+              '$_counter',
+              style: _textStyle,
+            )
+          ],
+        )),
+        /* floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, */
+        floatingActionButton: _createButtons());
+  }
+
+  Widget _createButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        const SizedBox(
+          width: 30,
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero_outlined),
+          onPressed: _resetCounter,
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          onPressed: _decreaseCounter,
+        ),
+        const SizedBox(
+          width: 12.0,
+        ),
+        FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () => setState(() {
-                _counter++;
-              })),
+          onPressed: _increaseCounter,
+        ),
+      ],
     );
   }
+
+  void _increaseCounter() => setState(() =>
+        _counter++
+      );
+  void _decreaseCounter() => setState(() =>
+        _counter--
+      );
+  void _resetCounter() => setState(() =>
+        _counter = 0
+      );
 }
